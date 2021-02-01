04300971fff5a7ce412a6b5aa1a87c37c42fd863
trevorsaudi <isatrevor052@gmail.com>
2021-02-01 16:54:35 +0300

    added some models, rendering the store products from the database

--- ecommerce/shoestore/urls.py
+++ ecommerce/shoestore/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from . import views 
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
 
 urlpatterns = [
   path('',views.home, name='home'),
@@ -7,4 +9,7 @@ urlpatterns = [
   path('cart/', views.cart, name='cart'), 
   path('checkout/', views.checkout, name='checkout'), 
   path('contact/',views.contact, name='contact')
+
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
