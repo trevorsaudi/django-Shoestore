397201714993ec7280cf7b832bb08087147aaddd
trevorsaudi <isatrevor052@gmail.com>
2021-02-22 04:43:53 -0500

    model changes

--- ecommerce/shoestore/models.py
+++ ecommerce/shoestore/models.py
@@ -6,15 +6,15 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
 	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)
-	email = models.CharField(max_length=200)
+	email = models.CharField(max_length=200, null=True)
 
 	def __str__(self):
 		return self.name
 
 
 class Product(models.Model):
-	name = models.CharField(max_length=200)
-	price = models.FloatField()
+	name = models.CharField(max_length=200,null=True)
+	price = models.FloatField(null=True)
 	digital = models.BooleanField(default=False,null=True, blank=True)
 	image = models.ImageField(null=True, blank=True)
 
