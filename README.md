26950d891245413966f711c61966f233d79de821
trevorsaudi <isatrevor052@gmail.com>
2021-02-22 04:51:35 -0500

    model changes

--- README.md
+++
@@ -1,2 +0,0 @@
-
-E-commerce site created in django for a shoe store company based in Kenya.
--- ecommerce/shoestore/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShoestoreConfig(AppConfig):
-    name = 'shoestore'
